972544bcb6767fec19157f0a2f7a80248ac76439
HarryLiow <harry.liow@1utar.my>
2022-04-04 15:27:11 +0800

    laptop

--- laptop.php
+++ laptop.php
@@ -7,8 +7,18 @@
         <?php $page = "laptop"; include('includes/navigation.php'); ?>
 
         <article>
-        <!--real content/body start here-->
-        
+        <div id="base" class="">
+
+      <!-- Unnamed (Group) -->
+      <div id="u179" class="ax_default" data-left="489" data-top="168" data-width="235" data-height="189">
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u180" class="ax_default box_2">
+          <div id="u180_div" class=""></div>
+          <div id="u180_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
+        </div>
         </article>
 
         <?php include('includes/footer.php'); ?>
