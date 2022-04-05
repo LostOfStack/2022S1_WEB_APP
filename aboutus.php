f12efb5bdd0e09076d9b277ff86000c0258c0d0e
LostOfStack <zhexuin@outlook.com>
2022-04-05 16:56:16 +0800

    aboutus, contactus (undone)

--- aboutus.php
+++ aboutus.php
@@ -2,17 +2,57 @@
 <html>
     <head>
     <title>About Us</title>
-    <?php 
-        include('includes/header.php');
-        $page = null;
-        include('includes/navigation.php');
+        <?php 
+            include('includes/header.php');
+            $page = null;
+            include('includes/navigation.php');
         ?>
+        <style>
+            .aboutus01{
+                width:100%;
+                background-color:#001719;
+                height:500px;
+                text-align:center;
+            }
+
+            .aboutus02{
+                margin-top: 50px;
+                margin-right: auto;
+                margin-left: auto;
+                padding: 20px;
+                position: static;
+                width: 50%;
+                border-style: solid;
+                border-color: black;
+                border-radius: 30px;
+                background-color: white;
+                text-align: left;
+                box-shadow: 5px 10px 5px #888888;
+                min-width: 400px;
+            }
+            
+        </style>
     </head>
     <body>
 
-        <div class=".main">
+        <div class="main">
             <h1>About Us</h1>
         </div>
+
+        <div class="aboutus01">
+            <img src="image/Creative.png" alt="Hardwareology">
+        <div>
+
+        <div class="aboutus02">
+            <h1 style="text-align:center">Hardwareology</h1>
+            <h2>History:</h2>
+            <p>Created in early first quater of 2022.<br>End in early second quater of 2022.<br>You ask for history? the headquater is combination of 4 computers only, once wiseman said <strong><em>"A lot of richest company, their headquater start from their own garage."</em></strong></p>
+            <h2>Mission:</h2>
+            <p><i>To maintain mankind live under technology, One person one purchase, keep my wallet noice and fat.</i></p>
+            <h2>Vision:</h2>
+            <p>Your money belongs to me, My money also belongs me</p>
+        </div>
+        
 
         <?php include('includes/footer.php'); ?>
     </body>
--- contactus.php
+++ contactus.php
@@ -2,19 +2,47 @@
 <html>
     <head>
     <title>Contact Us</title>
-    <?php 
-        include('includes/header.php');
-        $page = null;
-        include('includes/navigation.php');
+        <?php 
+            include('includes/header.php');
+            $page = null;
+            include('includes/navigation.php');
         ?>
+
+        <style>
+            .contactus01{
+                margin-top: 50px;
+                margin-right: auto;
+                padding: 20px;
+                position: static;
+                width: 40%;
+                border-style: solid;
+                border-color: black;
+                border-radius: 30px;
+                background-color: white;
+                text-align: left;
+                box-shadow: 5px 10px 5px #888888;
+                min-width: 400px;
+            }
+        </style>
     </head>
     <body>
 
-        <div>
+        <div class="main">
             <h1>Contact Us</h1>
-        
+            <img src="image/aboutus.jpg" alt="company picture" style="width:536px; height:356px;">
         </div>
+        <div class="contactus01">
+            <h1 style="text-align:center">Hardwareology</h1>
+            <p style="color:red">*Don't contact! It won't work! Nobody gonna to reply you!</p>
+            <p>Contact Number:<br>0123456789</p>
+            <br>
+            <p>Email:<br>hardwareology@gmail.com</p>
+            <br>
+            <p>Follow Us on our Facebook to get latest Update!<br><a href="https://www.facebook.com/UTARnet" target="_blank">Hardwareology</a></p>
+            <p style="color:red">&No such facebook page exist, we didn't create it, the link direct you to Utar Facebook page</p>
+            <Email>
 
+        </div>
         <?php include('includes/footer.php'); ?>
     </body>
 </html>
