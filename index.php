f08e17aff26c503580cb983aa11a10acf9c529fd
BennyKZL <kwanzhengle1003@gmail.com>
2022-04-06 03:36:11 +0800

    Merge branch 'main' of https://github.com/LostOfStack/2022S1_WEB_APP

--- index.php
+++ index.php
@@ -4,7 +4,31 @@
     <title>Home</title>
     </head>
     <body>
-        
+    <div class="myhome">
+    <h1 style="font-size:100px">Brand New Laptop</h1>
+        <div class="rowhome">
+            <div class="columnhome">
+            <a href="cart.php">
+            <img src="image/apple laptop.jpg" alt="AppleLaptop" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;position:relative;top:110px;">MacBookPro13"<br>RM 5,599<br><br>Feature:<br>Retina display<br>Apple M1 chip<br>Backlit Magic Keyboard<br>Two Thunderbolt / USB 4 ports with support<br><br>
+            </p>
+            </div>
+            <div class="columnhome">
+            <a href="cart.php">
+            <img src="image/acer laptop.jpg" alt="AcerLaptop" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;">Swift X AMD<br>RM4,299<br><br>Feature:<br>1.39kg<br>GeForce RTX 3050Ti<br>16gb memory<br><br></p>
+            </div>
+            <div class="columnhome">
+            <a href="cart.php">
+            <img src="image/dell laptop.jpg" alt="DellLaptop" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;position:relative;top:30px;">Dell XPS 13<br>RM6,979<br><br>Feature:<br>3200x1800 pixels<br>TouchScreen<br>128GB SSD Storage<br><br></p>
+            </div>
+        </div>
+</div>
+     
 
 
         
