ebb946f2a737fface739ff4a5a0af459f719f874
honwenxuan <honwenxuan@gmail.com>
2022-04-05 16:39:25 +0800

    added login

---
+++ signup.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Home</title>
+    </head>
+    <body>
+        <?php include('includes/guest header.php'); ?>
+
+
+
+        <?php include('includes/footer.php'); ?>
+    </body>
+</html>
