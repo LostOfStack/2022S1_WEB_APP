00e2c3640f20098057bb59ea1b606268ee22b457
LostOfStack <zhexuin@outlook.com>
2022-04-05 18:02:55 +0800

    contactus done

--- contactus.php
+++ contactus.php
@@ -10,11 +10,12 @@
 
         <style>
             .contactus01{
-                margin-top: 50px;
-                margin-right: auto;
+                float:left;
+                margin-left: 50px;
+                margin-right: 50px;
                 padding: 20px;
                 position: static;
-                width: 40%;
+                width: 60%;
                 border-style: solid;
                 border-color: black;
                 border-radius: 30px;
@@ -23,25 +24,51 @@
                 box-shadow: 5px 10px 5px #888888;
                 min-width: 400px;
             }
+
+            .contactus02{
+                margin: 50px;
+                float:none;
+                position:static;
+            }
+
+            .contactus03{
+                float:none;
+                margin-left:50px;
+                margin-right: auto;
+                padding: 20px;
+                position: static;
+                width: auto;
+                height: 600px;
+                text-align: center;
+                min-width: 400px;
+            }
         </style>
     </head>
     <body>
 
         <div class="main">
             <h1>Contact Us</h1>
-            <img src="image/aboutus.jpg" alt="company picture" style="width:536px; height:356px;">
         </div>
-        <div class="contactus01">
-            <h1 style="text-align:center">Hardwareology</h1>
-            <p style="color:red">*Don't contact! It won't work! Nobody gonna to reply you!</p>
-            <p>Contact Number:<br>0123456789</p>
-            <br>
-            <p>Email:<br>hardwareology@gmail.com</p>
-            <br>
-            <p>Follow Us on our Facebook to get latest Update!<br><a href="https://www.facebook.com/UTARnet" target="_blank">Hardwareology</a></p>
-            <p style="color:red">&No such facebook page exist, we didn't create it, the link direct you to Utar Facebook page</p>
-            <Email>
+        <div>
+            <div class="contactus01">
+                <h2 style="text-align:center">Hardwareology</h2>
+                <p style="color:red">*Don't contact! It won't work! Nobody gonna to reply you!</p>
+                <p>Contact Number:<br>0123456789</p>
+                <br>
+                <p>Email:<br>hardwareology@gmail.com</p>
+                <br>
+                <p>Follow Us on our Facebook to get latest Update!<br><a href="https://www.facebook.com/UTARnet" target="_blank">Hardwareology</a></p>
+                <p style="color:red">&No such facebook page exist, we didn't create it, the link direct you to Utar Facebook page</p>
+            </div>
 
+            <div class="contactus02">
+                <img src="image/aboutus.jpg" alt="company picture" style="width:536px; height:356px;">
+            </div>
+
+            <div class="contactus03">
+                <h2>Maps</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.2035003545775!2d101.79224896475706!3d3.0400444477861175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc34a5f21a8235%3A0x78796ffc32ce3fcd!2sUniversiti%20Tunku%20Abdul%20Rahman%20(UTAR)!5e0!3m2!1sen!2smy!4v1649149374019!5m2!1sen!2smy" width="700px" height="500px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
         <?php include('includes/footer.php'); ?>
     </body>
