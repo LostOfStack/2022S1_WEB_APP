7d6fe2acd5437d3786f93f942a32169008e8c36d
BennyKZL <kwanzhengle1003@gmail.com>
2022-04-07 03:04:15 +0800

    WHY choose us

--- index.php
+++ index.php
@@ -60,49 +60,94 @@ session_start();
 
     <div class="myhome">
         <h1>Home</h1>
-        <h1 style="font-size:100px">Brand New Laptop</h1>
+        <h1 style="font-size:100px;text-align:center;">Brand New Laptop</h1>
         <div class="rowhome">
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/apple laptop.jpg" alt="AppleLaptop" style="width:100%;height:100%;">
             </a>
-            <p style="font-size:50px;position:relative;top:110px;">MacBookPro13"<br>RM 5,599<br><br>Feature:<br>Retina display<br>Apple M1 chip<br>Backlit Magic Keyboard<br>Two Thunderbolt / USB 4 ports with support<br><br>
-            </p>
             </div>
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/acer laptop.jpg" alt="AcerLaptop" style="width:100%;height:100%;">
             </a>
-            <p style="font-size:50px;">Swift X AMD<br>RM4,299<br><br>Feature:<br>1.39kg<br>GeForce RTX 3050Ti<br>16gb memory<br><br></p>
             </div>
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/dell laptop.jpg" alt="DellLaptop" style="width:100%;height:100%;">
             </a>
-            <p style="font-size:50px;position:relative;top:30px;">Dell XPS 13<br>RM6,979<br><br>Feature:<br>3200x1800 pixels<br>TouchScreen<br>128GB SSD Storage<br><br></p>
             </div>
         </div>
     </div>
-        <h2 style="font-size:100px"><br><br>Other Product</h2>
+    <div class="rowhome">
+            <div class="columnhome">
+            <p style="font-size:50px;">MacBookPro13"<br>RM 5,599<br><br>Feature:<br>Retina display<br>Apple M1 chip<br>Backlit Magic Keyboard<br>Two Thunderbolt / USB 4 ports with support<br><br>
+            </p>
+            </div>
+            <div class="columnhome">
+            <p style="font-size:50px;">Swift X AMD<br>RM4,299<br><br>Feature:<br>1.39kg<br>GeForce RTX 3050Ti<br>16gb memory<br><br></p>
+            </div>
+            <div class="columnhome">
+            <p style="font-size:50px;position:relative;top:30px;">Dell XPS 13<br>RM6,979<br><br>Feature:<br>3200x1800 pixels<br>TouchScreen<br>128GB SSD Storage<br><br></p>
+            </div>
+        </div>
+        <h2 style="font-size:100px;text-align:center;">Other Product</h2>
     <div class="rowhome">
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=desktop">
             <img src="image/dell desktop.jpg" alt="DellDesktop" style="width:70%;height:100%;">
             </a>
-            <p style="font-size:50px;">Desktop
             </p>
             </div>
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=mouse">
             <img src="image/glorious mouse.jpg" alt="Mouse" style="width:70%;height:100%;">
             </a>
-            <p style="font-size:50px;">Mouse</p>
             </div>
             <div class="columnhome">
             <a href="/2022S1_WEB_APP/product.php?page=speaker">
             <img src="image/jbl speaker.jpg" alt="Speaker" style="width:70%;height:100%;">
             </a>
+            </div>
+        </div>
+        <div class="rowhome">
+            <div class="columnhome">
+            <p style="font-size:50px;">Desktop
+            </p>
+            </div>
+            <div class="columnhome">
+            <p style="font-size:50px;">Mouse</p>
+            </div>
+            <div class="columnhome">
             <p style="font-size:50px;">Speaker</p>
+            </div>
+        </div>
+        <h2 style="font-size:100px;text-align:center;">Why Choose Us</h2>
+        <div class="rowhome">
+            <div class="columnhome">
+            <img src="image/cash2.png" alt="CashLogo" style="width:70%;height:100%;">
+            </a>
+            </p>
+            </div>
+            <div class="columnhome">
+            <img src="image/Clock.png" alt="CashLogo" style="width:40%;height:100%;">
+            </a>
+            </div>
+            <div class="columnhome">
+            <img src="image/suitcase.png" alt="CashLogo" style="width:40%;height:100%;">
+            </a>
+            </div>
+        </div>
+        <div class="rowhome">
+            <div class="columnhome">
+            <p style="font-size:50px;">Low Cost
+            </p>
+            </div>
+            <div class="columnhome">
+            <p style="font-size:50px;">Fast</p>
+            </div>
+            <div class="columnhome">
+            <p style="font-size:50px;">Professional</p>
             </div>
         </div>
     <style>
