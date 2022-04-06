638aa75ef2b6f10228d2e85dca935839c9ac157e
BennyKZL <kwanzhengle1003@gmail.com>
2022-04-07 02:27:05 +0800

    home page

--- index.php
+++ index.php
@@ -63,26 +63,47 @@ session_start();
         <h1 style="font-size:100px">Brand New Laptop</h1>
         <div class="rowhome">
             <div class="columnhome">
-            <a href="cart.php">
+            <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/apple laptop.jpg" alt="AppleLaptop" style="width:100%;height:100%;">
             </a>
-            <p style="font-size:50px;position:static;top:110px;">MacBookPro13"<br>RM 5,599<br><br>Feature:<br>Retina display<br>Apple M1 chip<br>Backlit Magic Keyboard<br>Two Thunderbolt / USB 4 ports with support<br><br>
+            <p style="font-size:50px;position:relative;top:110px;">MacBookPro13"<br>RM 5,599<br><br>Feature:<br>Retina display<br>Apple M1 chip<br>Backlit Magic Keyboard<br>Two Thunderbolt / USB 4 ports with support<br><br>
             </p>
             </div>
             <div class="columnhome">
-            <a href="cart.php">
+            <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/acer laptop.jpg" alt="AcerLaptop" style="width:100%;height:100%;">
             </a>
             <p style="font-size:50px;">Swift X AMD<br>RM4,299<br><br>Feature:<br>1.39kg<br>GeForce RTX 3050Ti<br>16gb memory<br><br></p>
             </div>
             <div class="columnhome">
-            <a href="cart.php">
+            <a href="/2022S1_WEB_APP/product.php?page=laptop">
             <img src="image/dell laptop.jpg" alt="DellLaptop" style="width:100%;height:100%;">
             </a>
-            <p style="font-size:50px;position:static;top:30px;">Dell XPS 13<br>RM6,979<br><br>Feature:<br>3200x1800 pixels<br>TouchScreen<br>128GB SSD Storage<br><br></p>
+            <p style="font-size:50px;position:relative;top:30px;">Dell XPS 13<br>RM6,979<br><br>Feature:<br>3200x1800 pixels<br>TouchScreen<br>128GB SSD Storage<br><br></p>
             </div>
         </div>
     </div>
+    <div class="rowhome">
+            <div class="columnhome">
+            <a href="/2022S1_WEB_APP/product.php?page=desktop">
+            <img src="image/dell desktop.jpg" alt="DellDesktop" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;position:relative;top:80px;">Desktop
+            </p>
+            </div>
+            <div class="columnhome">
+            <a href="/2022S1_WEB_APP/product.php?page=mouse">
+            <img src="image/glorious mouse.jpg" alt="Mouse" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;">Mouse</p>
+            </div>
+            <div class="columnhome">
+            <a href="/2022S1_WEB_APP/product.php?page=speaker">
+            <img src="image/jbl speaker.jpg" alt="Speaker" style="width:100%;height:100%;">
+            </a>
+            <p style="font-size:50px;position:relative;top:200px;">Speaker</p>
+            </div>
+        </div>
     <style>
         .wrapper{
             margin-top: 150px;
