0171e5c2b9d11ab50e71d5f3bcc0293b485d300d
HarryLiow <harry.liow@1utar.my>
2022-04-07 16:33:55 +0800

    1

--- cart.php
+++ cart.php
@@ -1,4 +1,26 @@
-
+<?php
+    session_start();
+    require 'config.php';
+    if(!isset($_SESSION["loggedin"])){
+        header('location: login.php');
+    }
+    $user_id=$_SESSION['id'];
+    $user_products_query="select it.id,it.name,it.price from user_items ut inner join product it on it.id=ut.item_id where ut.user_id='$user_id'";
+    $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+    $no_of_user_products= mysqli_num_rows($user_products_result);
+    $sum=0;
+    if($no_of_user_products==0){
+    ?>
+        <script>
+        window.alert("No items in the cart!!");
+        </script>
+    <?php
+    }else{
+        while($row=mysqli_fetch_array($user_products_result)){
+            $sum=$sum+$row['price']; 
+       }
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,22 +32,32 @@
         
         <h1>Shopping Cart</h1>
 
-        <table width='80%' border = 1 >
-
-            <tr>
-                <th>Subject</th> <th>Message</th> <th>Type</th> <th>Posted Date</th> <th> Action </th>
-            </tr>
-            <?php  
-                while($row = mysqli_fetch_array($result)) {         
-                    echo "<tr>";
-                    echo "<td>".$row['subject']."</td>";
-                    echo "<td>".$row['message']."</td>";
-                    echo "<td>".$row['type']."</td>"; 
-                    echo "<td>".$row['posted']."</td>";		
-                    echo "<td><a href='edit.php?id=".$row['id']."'>Edit</a> | <a href='delete.php?id=".$row['id']."'>Delete</a></td></tr>";        
-                }
-            ?>
-        </table>
+        <br>
+            <div class="container">
+                <table class="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <th>Item Number</th><th>Item Name</th><th>Price</th><th></th>
+                        </tr>
+                       <?php 
+                        $user_products_result=mysqli_query($con,$user_products_query) or die(mysqli_error($con));
+                        $no_of_user_products= mysqli_num_rows($user_products_result);
+                        $counter=1;
+                       while($row=mysqli_fetch_array($user_products_result)){
+                           
+                         ?>
+                        <tr>
+                            <th><?php echo $counter ?></th><th><?php echo $row['name']?></th><th><?php echo $row['price']?></th>
+                            <th><a href='cart_remove.php?id=<?php echo $row['id'] ?>'>Remove</a></th>
+                        </tr>
+                       <?php $counter=$counter+1;}?>
+                        <tr>
+                            <th></th><th>Total</th><th>Rs <?php echo $sum;?>/-</th><th><a href="success.php?id=<?php echo $user_id?>" class="btn btn-primary">Confirm Order</a></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br><br><br><br><br><br><br><br><br><br>
 
         <?php include('includes/footer.php'); ?>
     </body>
--- cart_add.php
+++ cart_add.php
@@ -6,5 +6,5 @@
     $user_id=$_SESSION['id'];
     $add_to_cart_query="insert into user_items(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
     $add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
-    header('location: product.php');
+    header('location: cart.php');
 ?>
