d9cda0026d9bdeff1fb376f1c5e156207acbebe4
HarryLiow <harry.liow@1utar.my>
2022-04-08 18:25:00 +0800

    1

--- product.php
+++ product.php
@@ -70,6 +70,75 @@ $numRow = mysqli_num_rows($result);
                 height:380px;
                 width:600px;
             }
+            .btn{
+                width: 300px;
+                height: 100px;
+                border: none;
+                color: white;
+                background-color: #8a8f94;
+                border-radius: 4px;
+                box-shadow: inset 0 0 0 0 #f9e506;
+                font-size:1.5rem;
+                padding: 6px;
+                outline:none;
+            }
+           
+            .btn, a:link { 
+                text-decoration: none; 
+            }
+
+            .btn-success {
+            color: #fff;
+            background-color: #5cb85c;
+            border-color: #4cae4c;
+            }
+            .btn-success:focus,{
+            color: #fff;
+            background-color: #449d44;
+            border-color: #255625;
+            }
+            .btn-success:hover {
+            color: #fff;
+            background-color: #449d44;
+            border-color: #398439;
+            }
+            .btn-success:active{
+            color: #fff;
+            background-color: #449d44;
+            border-color: #398439;
+            }
+            .btn-success:active:hover {
+            color: #fff;
+            background-color: #398439;
+            border-color: #255625;
+            }
+        
+            .btn-primary {
+            color: #fff;
+            background-color: #0a0a0a;
+            border-color: #2e6da4;
+            }
+            .btn-primary:focus{
+            color: #fff;
+            background-color:#27272b;
+            border-color: #27272b;
+            }
+            .btn-primary:hover {
+            color: #fff;
+            background-color: #27272b;
+            border-color: #27272b;
+            }
+            .btn-primary:active,{
+            color: #fff;
+            background-color: #27272b;
+            border-color:#27272b;
+            }
+            .btn-primary:active:hover{
+            color: #fff;
+            background-color: #27272b;
+            border-color:#27272b;
+            }
+     
             
         </style>
     </head>
@@ -102,15 +171,15 @@ $numRow = mysqli_num_rows($result);
                             <div class="ptextbox2">
                                 <h3>Price: RM'.$row['price'].'</h3>';
                                 if(!isset($_SESSION["loggedin"])){
-                                    echo '<p><a href="login.php" role="button" class="btn btn-primary btn-block ">Buy Now</a></p>';
+                                    echo '<p><a href="login.php" role="button" class="btn btn-primary >Buy Now</a></p>';
                                 }else{
                                     if(check_if_added_to_cart($row['id'])){
-                                        echo '<a href="#" class=btn btn-success disabled>Added to cart</a>';
+                                        echo '<a href="#" class=btn btn-success disabled >Added to cart</a>';
                                     }else{
                                         if(check_if_added_to_cart($row['id'])){
-                                            echo '<a href="#" class=btn btn-success disabled>Added to cart</a>';
+                                            echo '<a href="#" class= btn btn-success disabled >Added to cart</a>';
                                         }else{
-                                            echo '<a href="cart_add.php?id='.$row['id'].'" class="btn btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>';
+                                            echo '<a href="cart_add.php?id='.$row['id'].'" class="btn btn-primary " name="add" value="add" class="btn btn-primary ">Add to cart</a>';
                                         }
                                     }
                                 }
