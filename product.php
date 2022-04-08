4f5a4d2f11e807b6fc339189fbd421a1aa90a49e
LostOfStack <zhexuin@outlook.com>
2022-04-08 21:16:45 +0800

    yeah

--- product.php
+++ product.php
@@ -171,7 +171,7 @@ $numRow = mysqli_num_rows($result);
                             <div class="ptextbox2">
                                 <h3>Price: RM'.$row['price'].'</h3>';
                                 if(!isset($_SESSION["loggedin"])){
-                                    echo '<p><a href="login.php" role="button" class="btn btn-primary >Buy Now</a></p>';
+                                    echo '<p><a href="login.php" role="button" class="btn btn-primary" >Buy Now</a></p>';
                                 }else{
                                     if(check_if_added_to_cart($row['id'])){
                                         echo '<a href="#" class=btn btn-success disabled >Added to cart</a>';
