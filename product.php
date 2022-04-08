e30f23a46b9ddc46f2aa81f8180efaeed2a2faa5
HarryLiow <harry.liow@1utar.my>
2022-04-08 13:26:25 +0800

    1

--- product.php
+++ product.php
@@ -103,7 +103,7 @@ $numRow = mysqli_num_rows($result);
                                 if(!isset($_SESSION["loggedin"])){
                                     echo '<p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>';
                                 }else{
-                                    if(check_if_added_to_cart($row1['id'])){
+                                    if(check_if_added_to_cart($row['id'])){
                                         echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
                                     }else{
                                         if(check_if_added_to_cart($row['id'])){
